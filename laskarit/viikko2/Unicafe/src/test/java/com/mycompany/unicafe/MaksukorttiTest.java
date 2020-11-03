2fae7b1b636b0fd1c93e21af203d32710b2e5807
Leo Becker <leijonamieli@hotmail.fi>
2020-11-04 00:26:33 +0200

    ööö

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,38 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void lataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString());
+    }
+    
+    @Test
+    public void ottaminenPienentaaSaldoaOikein() {
+        kortti.otaRahaa(5);
+        // Tarkistetaan että bugi on vieläkin olemassa
+        assertEquals("saldo: 0.5", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosEiRiita() {
+        kortti.otaRahaa(15);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaTrue() {
+        assertTrue(kortti.otaRahaa(5));
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaFalse() {
+        assertTrue(!kortti.otaRahaa(15));
+    }
 }
